dae195f597695b2d6235df3cf9d3e45d9455a2c6
Denis Sidorenko <dongor7@gmail.com>
2018-06-29 10:37:53 +0300

    Initial commit

---
+++ backend/controllers/todos.js
@@ -0,0 +1,36 @@
+const Todo = require('../models/todo');
+
+async function findAll(ctx) {
+  const todos = await Todo.find({});
+  ctx.body = todos;
+}
+
+async function create(ctx) {
+  const newTodo = new Todo(ctx.request.body);
+  const savedTodo = await newTodo.save();
+  ctx.body = savedTodo;
+}
+
+async function destroy (ctx) {
+  const id = ctx.params.id;
+  const todo = await Todo.findById(id);
+
+  const deletedTodo = await todo.remove();
+  ctx.body = deletedTodo;
+}
+
+async function update(ctx) {
+  const id = ctx.params.id;
+  const todo = await Todo.findById(id);
+  todo.done = !todo.done;
+
+  const updateTodo = await todo.save();
+  ctx.body = updateTodo;
+}
+
+module.exports = {
+  findAll,
+  create,
+  destroy,
+  update
+};

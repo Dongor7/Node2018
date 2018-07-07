56ea5005f576617f1cec4deedc6ba965829aa56e
Denis Sidorenko <dongor7@gmail.com>
2018-07-07 22:12:05 +0300

    FETCH_FAIL corrected *

--- frontend/src/sagas/index.js
+++ frontend/src/sagas/index.js
@@ -5,7 +5,7 @@ import { FETCH_TODOS, fetchTodosSuccess, fetchTodosFail } from "../actions/todos
 
 function* fetchTodos() {
     try {
-        const response = yield call(() => axios.get('http://localhost:4000/v1/todoss/'));
+        const response = yield call(() => axios.get('http://localhost:4000/v1/todoss/')); // правильный ..../todos/
         yield put(fetchTodosSuccess(response.data));
     } catch (e) {
         yield put(fetchTodosFail(e.message));

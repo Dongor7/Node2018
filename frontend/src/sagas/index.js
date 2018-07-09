0db29357e844a4ce588c5f0ea4594dc97d20cfc3
Denis Sidorenko Workplace <dongor7@gmail.com>
2018-07-09 17:31:14 +0300

    Add todo

--- frontend/src/sagas/index.js
+++ frontend/src/sagas/index.js
@@ -1,20 +1,33 @@
 import axios from 'axios';
 import { call, put, takeLatest, takeEvery, all} from 'redux-saga/effects';
-import { FETCH_TODOS, DELETE_TODO, fetchTodosSuccess, fetchTodosFail, deleteTodo } from "../actions/todos";
+import { FETCH_TODOS, DELETE_TODO, ADD_TODO, fetchTodosSuccess, fetchTodosFail} from "../actions/todos";
 
 
 function* fetchTodos() {
     try {
-        const response = yield call(() => axios.get('http://localhost:4000/v1/todos/'));
+        const response = yield call(() => axios.get('http://localhost:8000/v1/todos/'));
         yield put(fetchTodosSuccess(response.data));
     } catch (e) {
         yield put(fetchTodosFail(e.message));
     }
 }
 
-function* fetchDeleteTodo(action) {
+function* fetchDeleteTodo({id}) {
     try {
-        yield call(() => axios.delete(`http://localhost:4000/v1/todos/${action.id}`));
+        yield call(() => axios.delete(`http://localhost:8000/v1/todos/${id}`));
+    } catch (e) {
+        yield put(fetchTodosFail(e.message));
+    }
+}
+
+function* fetchAddTodo({todo}) {
+    try {
+        yield call(() => axios.post('http://localhost:8000/v1/todos/', {
+            todo: {
+                description: todo.description,
+                complete: todo.complete
+            }
+        }));
     } catch (e) {
         yield put(fetchTodosFail(e.message));
     }
@@ -23,7 +36,8 @@ function* fetchDeleteTodo(action) {
 function* mySaga() {
     yield all([
         takeLatest(FETCH_TODOS, fetchTodos),
-        takeEvery(DELETE_TODO, fetchDeleteTodo)
+        takeEvery(DELETE_TODO, fetchDeleteTodo),
+        takeEvery(ADD_TODO, fetchAddTodo)
     ]);
 }
 
